4495f365df85dc0da15343ab4342d766e68a9a2a
Carlos Fernandez <CarlosFdez@users.noreply.github.com>
2017-08-18 18:54:57 -0400

    string parser improvements, more jsdoc comments

--- examples/simplebot.js
+++ examples/simplebot.js
@@ -6,7 +6,7 @@ var config = require("./config.json");
 
 // config options are the same as normal tmi.client
 var client = new tmi.client(config.tmi);
-var bot = tmibot.create_bot(client);
+var bot = tmibot.create_bot(client, "!");
 
 bot.addCommand("ping", (ctx, args) => {
     ctx.reply("pong");
